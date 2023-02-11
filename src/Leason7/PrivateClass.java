c8612ab43f9122899a7401b50d0c90d93e383799
Barash Anton <BarashASUA@gmail.com>
2023-02-12 01:06:04 +0200

    Public class

---
+++ src/Leason7/PrivateClass.java
@@ -0,0 +1,7 @@
+package Leason7;
+
+public class PrivateClass {
+    public static void main(String[] args) {
+        System.out.println("Hi , I am private class , and I doesn't visible for You");
+    }
+}
